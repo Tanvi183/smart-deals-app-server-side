7aff671191bef824dee8075b1dc3ba558fa06807
tanvi183 <mneshat7@gmail.com>
2025-10-31 22:17:58 -0700

    connect with database and store data in database with post method.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,10 +8,58 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://smartDealsDBUser:MSKT2W4dkGeYVs7S@simple-crud-server.7fhuvu7.mongodb.net/?appName=simple-crud-server";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Smart server is running");
 });
 
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    const db = client.db("smart_deals_db");
+    const productCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log(`smart server is running ${port}`);
+  console.log(`smart server is running ${port}`);
 });
+
+
+
+// differen way connect database
+// client
+//   .connect()
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`smart server is running ${port}`);
+//     });
+//   })
+//   .catch(console.dir);
